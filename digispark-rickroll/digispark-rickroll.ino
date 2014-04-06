4294ebc3aab0d1b319b7c5650f9b969f6a043f9c
Quinn Rohlf <qr@qrohlf.com>
2014-04-06 11:16:21 -0700

    interval rickroll and volume control

--- digispark-rickroll/digispark-rickroll.ino
+++ digispark-rickroll/digispark-rickroll.ino
@@ -1,5 +1,12 @@
 #include <DigiKeyboard.h>
 #define PIN_LED 1
+#define INTERVAL_MIN 60*5  // The minimum time between rickrolls, in seconds
+#define INTERVAL_MAX 60*7  // The minimum time between rickrolls, in seconds
+
+// key codes from p53 of http://www.usb.org/developers/devclass_docs/Hut1_11.pdf
+#define KEY_FWDSLASH 56
+#define KEY_VOLUP   128
+#define KEY_VOLDN   129
 
 void blink(int times) {
   for (int i=0; i<times; i++) {
@@ -8,6 +15,17 @@ void blink(int times) {
     digitalWrite(PIN_LED, LOW);    // turn the LED off by making the voltage LOW
     delay(250);
   }
+}
+
+// Perform apple keyboard calibration sequence
+void calibrateKeyboard() {
+  // 5 seconds to press continue button
+  DigiKeyboard.delay(5000);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_Z);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_FWDSLASH);
 }
 
 // Send the key sequence to open a rickroll
@@ -24,11 +42,13 @@ void rickroll() {
   // Open a rickroll
   DigiKeyboard.sendKeyStroke(KEY_T, MOD_GUI_LEFT);
   DigiKeyboard.delay(300);
-  DigiKeyboard.print("http://bit.ly/1e35aW9");
+  DigiKeyboard.print("http://bit.ly/1fYriw1");
   DigiKeyboard.delay(300); // todo find a way to do this without spotlight
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.print("f");
+  for (int i=0; i<10; i++) {
+    DigiKeyboard.delay(10);
+    DigiKeyboard.sendKeyStroke(KEY_VOLUP);
+  }
   //signal rickroll sequence complete
   blink(3);
 }
@@ -36,9 +56,16 @@ void rickroll() {
 void setup() {
   //setup LED as output
   pinMode(PIN_LED, OUTPUT); //LED on Model B
+  //do apple keyboard setup
+  calibrateKeyboard();
+  //5 seconds to dismiss setup window
+  DigiKeyboard.delay(5000);
+  //rickroll immediately for testing purposes
   rickroll();
 }
 
 void loop() {
-  //nada for now
+  // double sec = INTERVAL_MIN + (rand() % (INTERVAL_MAX - INTERVAL_MIN));
+  // DigiKeyboard.delay(sec*1000);
+  // rickroll();
 }
